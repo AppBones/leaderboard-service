15f260f8b7398eaf6f3fa2b39c42326b87e613d1
Morten Faarkrog <mortenfaarkrog@gmail.com>
2016-03-30 23:22:07 +0200

    Service setup using the webservice-template. Liberator resources currently empty.

--- src/leaderboard_service/leaderboard.clj
+++ src/leaderboard_service/leaderboard.clj
@@ -1,64 +1,17 @@
 (ns leaderboard_service.leaderboard
-  (:require [ring.util.response :as r]
-            [leaderboard_service.util :as u]))
+  (:require [leaderboard_service.db :as db]
+            [liberator.core :refer [resource]]
+            [leaderboard_service.api :refer :all]))
 
 
-; ### Path: "api/v1/leaderboards"
+(defn leaderboards [ctx db path]
+  (let [spec (get-in ctx [:swagger :context :definition "paths" path])
+        handler
+        (resource)]
+  (handler ctx)))
 
-(defn list-leaderboards
-  ; Returns all leaderboards within a specified range
-  [request]
-  (let [{limit :limit, page :page} (u/get-query request)]
-    (-> (r/response {:limit limit :page page})
-        (r/content-type "application/json"))))
-
-(defn create-leaderboard
-  ; Adds a new leaderboard
-  [request]
-  (let [leaderboard (u/get-body request)]
-    (-> (r/response leaderboard)
-        (r/status 201)
-        (r/content-type "application/json"))))
-
-(defn list-options
-  ; Lists all valid HTTP verbs for "/api/v1/leaderboards"
-  [request]
-  (let [options "GET, POST, OPTIONS"]
-    (-> (r/response nil)
-        (r/header "Allow" options)
-        (r/content-type "application/json"))))
-
-
-; ### Path: "api/v1/leaderboards/{board_id}"
-
-(defn fetch-leaderboard
-  ; Returns a leaderboard with a given ID
-  [request]
-  (let [{board-id :board_id} (u/get-path request)]
-    (-> (r/response {:board-id board-id})
-        (r/content-type "application/json"))))
-
-(defn update-leaderboard
-  ; Updates a leaderboard with a given ID
-  [request]
-  (let [board-id (:board_id (u/get-path request))
-        leaderboard (u/get-body request)]
-    (-> (r/response leaderboard)
-        (r/status 204)
-        (r/content-type "application/json"))))
-
-(defn delete-leaderboard
-  ; Deletes a leaderboard with a given ID
-  [request]
-  (let [{board-id :board_id} (u/get-path request)]
-    (-> (r/response nil)
-        (r/status 202)
-        (r/content-type "application/json"))))
-
-(defn list-options-board
-  ; Lists all valid HTTP verbs for "/api/v1/leaderboards/{board_id}"
-  [request]
-  (let [options "GET, PATCH, DELETE, OPTIONS"]
-    (-> (r/response nil)
-        (r/header "Allow" options)
-        (r/content-type "application/json"))))
+(defn leaderboard [ctx db path]
+  (let [spec (get-in ctx [:swagger :context :definition "paths" path])
+        handler
+        (resource)]
+  (handler ctx)))
